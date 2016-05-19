2127a8c8e83dd20e32a650a4c6c0cc94742892d5
liuqiuchen <2016968116@qq.com>
2016-05-19 23:21:14 +0800

    commit form webstorm

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+$(function () {
 
+	/**
+	 * 设置头部为蓝色样式
+	 * */
+	$('ion-header-bar').addClass('bar-positive');
+
+
+	/**
+	 * 底部 tab 选项卡操作
+	 * */
+	var footerTab = {
+		listItems: '#myFooter ul.footer_nav_list li'
+	};
+	$(footerTab.listItems).on('tap', function () {
+		//console.log($(this).index());
+
+		$('.cont').css('display', 'none');
+		$('.cont' + ($(this).index() + 1)).fadeIn();
+	});
+
+});
 
 
 
